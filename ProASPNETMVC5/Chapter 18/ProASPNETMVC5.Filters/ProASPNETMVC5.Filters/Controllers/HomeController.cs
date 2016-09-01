ecbaeba3ad9cab3893b7cd9c903814f9f4b0c2b3
Xiang Lei Wang <xiangleiwang@outlook.com>
2016-09-01 18:54:02 +0800

    Add ProASPNETMVC5 folder

---
+++ ProASPNETMVC5/Chapter 18/ProASPNETMVC5.Filters/ProASPNETMVC5.Filters/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using ProASPNETMVC5.Filters.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProASPNETMVC5.Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize(Users ="admin")]
+        public string Index()
+        {
+            return "This is Index action on the Home controller";
+        }
+    }
+}
---
+++ ProASPNETMVC5/Chapter 18/ProASPNETMVC5.Filters/ProASPNETMVC5.Filters/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProASPNETMVC5.Filters.MvcApplication" Language="C#" %>
